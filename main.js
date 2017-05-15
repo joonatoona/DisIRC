145a3050f3814e5c9308761884160f48dbae6cf7
Joona <joonatoona@digitalfishfun.com>
2017-05-14 21:43:58 -0700

    more work on models

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fs = require('fs'), DisIRCSettings = require(__dirname + '/models/settings');
+const DisIRCBridge = require(__dirname + '/models/bridge'), DisIRCSettings = require(__dirname + '/models/settings');
 
 console.log(__dirname)
 
@@ -12,18 +12,3 @@ if (!fs.exists(__dirname + "/options.json")) {
 
 const settings = new DisIRCSettings(__dirname + "/options.json").settings;
 
-const discordClient = new Discord.Client();
-const ircClient = new irc.client()
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.username}!`);
-    client.generateInvite(["READ_MESSAGES", "SEND_MESSAGES"]).then((link) => {
-        console.log(link);
-    })
-});
-
-client.on('message', msg => {
-
-});
-
-client.login(fs.readFileSync('token', 'utf8'));
--- models/bridge.js
+++ models/bridge.js
@@ -2,8 +2,29 @@ const irc = require('irc'), Discord = require("discord.js");
 
 class DisIRC_Bridge {
     constructor(settings) {
+        this.settings = settings;
         this.discord_client = new Discord.Client();
-        this.irc_client = new irc.Client(settings.IRC.server);
+        this.discord_client.on('ready', this.onClientConnect);
+        this.discord_client.on('message', this.onMessage_Discord);
+        this.discord_client.login(this.settings.Discord.token)
+    }
+
+    onClientConnect() {
+        console.log("Connected to discord!")
+        this.irc_client = irc.Client(this.settings.IRC.server, this.discord_client.user.username);
+        this.irc_client.addListener('message', this.onMessage_IRC);
+        for (let channel of settings.ChannelMap) {
+            console.log("Joining " + channel)
+            this.irc_client.join(channel);
+        }
+    }
+
+    onMessage_Discord(msg) {
+
+    }
+
+    onMessage_IRC(from, to, msg) {
+
     }
 }
 
